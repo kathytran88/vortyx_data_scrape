d4212ee471788c0415d157d61b62c725bbe63448
kathytran88 <kathyngananhtran@gmail.com>
2024-08-23 18:53:46 -0400

    added files

---
+++ biz_owner/nhap.py
@@ -0,0 +1,10 @@
+    (40.7128, -74.0060),  # New York City
+    (34.0522, -118.2437), # Los Angeles
+    (41.8781, -87.6298),  # Chicago
+    (29.7604, -95.3698),  # Houston
+    (47.6062, -122.3321), # Seattle
+    (25.7617, -80.1918),  # Miami
+    (39.7392, -104.9903), # Denver
+
+
+'AIzaSyBeIS9MPnXxndKeJuiopxKGxn9TU2lUoFU'
